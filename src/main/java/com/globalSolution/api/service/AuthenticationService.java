bbf5d528c905a1675e6fbaa92d2a03022d497291
IagoCoelho <iagocoelhos@gmail.com>
2023-05-30 18:13:58 -0300

    authentication

---
+++ src/main/java/com/globalSolution/api/service/AuthenticationService.java
@@ -0,0 +1,12 @@
+package com.globalSolution.api.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService implements UserDetailsService{
+    
+    @Autowired
+    
+}
