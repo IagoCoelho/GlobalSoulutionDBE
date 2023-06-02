1c17dd2e7e9c19b65590fda24e1cf0dd54d59539
IagoCoelho <rm95683@fiap.com.br>
2023-06-02 20:09:17 -0300

    modificações

--- src/main/java/com/globalSolution/api/models/Cultivos.java
+++ src/main/java/com/globalSolution/api/models/Cultivos.java
@@ -5,6 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -18,6 +19,9 @@ public class Cultivos {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank
+    private String ds_resultados;
 
     @NotNull
     @ManyToOne
