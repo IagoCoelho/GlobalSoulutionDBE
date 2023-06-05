6112dd2da6cacf66e9160fefd01a0f0c612d6974
Logon Aluno <logonrmlocal@fiap.com.br>
2023-06-05 19:46:01 -0300

    INICIADO

--- src/main/java/com/globalSolution/api/config/DatabaseSeeder.java
+++ src/main/java/com/globalSolution/api/config/DatabaseSeeder.java
@@ -8,5 +8,23 @@ import org.springframework.context.annotation.Configuration;
 public class DatabaseSeeder implements CommandLineRunner{
     
     @Autowired
-    
+    TiposClimaRepository tipoClimaRepository;
+    TiposSoloRepository tipoSoloRepository;
+    GraoRepository graoRepository;
+    CultivosRepository cultivosRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        documentosRepository.saveAll(List.of(
+            new Documentos(1L, 543345543345, "x", "Registro geral", "Emitido,dia,:,8"),
+            new Documentos(1L, 768876678876, "y", "Registro geral", "Emitido,dia,:,9")
+
+        ))
+        categoriasRepository.saveAll(List.of(
+            new Categorias(1L, 1, "white", "Certificados", getDocId(1L,2L)),
+            new Categorias(1L, 1, "white", "Certificados", getDocId(1L,2L)),
+            new Categorias(1L, 1, "white", "Certificados", getDocId(1L,2L))
+        ));
+        
+    }
 }
