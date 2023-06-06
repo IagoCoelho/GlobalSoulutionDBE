29af45ead302bf42ae745d563ef298bfcf11cde4
jhionisio <mr.joaomarcelo@gmail.com>
2023-06-06 14:22:39 -0300

    AJUSTE NOS IMPORTS

--- src/main/java/com/globalSolution/api/ApiApplication.java
+++ src/main/java/com/globalSolution/api/ApiApplication.java
@@ -7,7 +7,30 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiApplication.class, args);
+		try {
+            EntityManagerFactory entityManagerFactory = Persistence.
+                    createEntityManagerFactory("TESTEGS-PU");
+            EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+            var customerRepository = new CustomerRepository(entityManager);
+            var tellerRepository = new TellerRepository(entityManager);
+
+            var novoCustomer = new Customer("Teste", "RuaTeste", 43434, 434334);
+            customerRepository.create(novoCustomer);
+            var customer = customerRepository.findById(1);
+
+            novoCustomer.setName("Teste2");
+            customerRepository.update(novoCustomer);
+            var customerUpdate = customerRepository.findById(1);
+
+            customerRepository.deleteById(1);
+            var customerDeleted = customerRepository.findById(1);
+
+            entityManager.close();
+            entityManagerFactory.close();
+        } catch (Exception e) {
+            throw e;
+        }
 	}
 
 }
--- src/main/java/com/globalSolution/api/repository/TipoSoloRepository.java
+++ src/main/java/com/globalSolution/api/repository/TipoSoloRepository.java
@@ -8,4 +8,14 @@ import com.globalSolution.api.models.TipoSolo;
 
 public interface TipoSoloRepository extends JpaRepository<TipoSolo, Long>{
     Page<TipoSolo> findByNameContaining(String solo, Pageable pageable);
+
+    public Iterable<TipoSolo> findByLike(String tipoSolo) {
+        String jpql = "SELECT d FROM TB_TIPO_SOLO d WHERE d.tipoSolo LIKE :tipoSolo";
+        var query = entityManager.createQuery(jpql, TipoSolo.class)
+                .setParameter("tipoSolo", "%" + tipoSolo + "%")
+                .setHint("jakarta.persistence.query.timeout", 60000);
+        var tipoSolo = query.getResultList();
+        return tipoSolo;
+    }
+
 }
